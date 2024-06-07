78f0cc487384a5cd2a320155c8896f24839942da
wanghanlin <wanghanlin@hailiang.com>
2024-06-07 17:19:22 +0800

    [init]:提示词模版

--- src/test/java/com/hailiang/langchain4jdemo/Langchain4JApplicationTests.java
+++ src/test/java/com/hailiang/langchain4jdemo/Langchain4JApplicationTests.java
@@ -4,11 +4,15 @@ import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.StreamingResponseHandler;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.annotation.Async;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class Langchain4JApplicationTests {
@@ -33,7 +37,7 @@ class Langchain4JApplicationTests {
      */
     @Test
     void TestStreamChatModel() {
-        streamingChatModel.generate("帮我写一个计算器的js代码", new StreamingResponseHandler<AiMessage>() {
+        streamingChatModel.generate("帮我用java写一个冒泡排序", new StreamingResponseHandler<AiMessage>() {
             @Override
             public void onNext(String token) {
                 System.out.println("onNext(): " + token);
@@ -49,6 +53,28 @@ class Langchain4JApplicationTests {
                 System.out.println("onError(): " + error);
             }
         });
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 动态提示词
+     */
+    @Test
+    void TestPromptTemplate() {
+        PromptTemplate promptTemplate = PromptTemplate.from("说 {{context}} 用 {{language}}.");
+        Map<String,Object> variables = new HashMap<>();
+        variables.put("context","你好");
+        variables.put("language","英语");
+        Prompt prompt = promptTemplate.apply(variables);
+
+
+        System.out.println(prompt.text());
     }
 
 }
