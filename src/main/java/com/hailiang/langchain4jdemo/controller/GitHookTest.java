858cdcc5ef0ac105e7a7940adc5f2f94287adbac
wanghanlin <wanghanlin@hailiang.com>
2024-06-17 17:28:04 +0800

    [init]:测试git-hook

--- src/main/java/com/hailiang/langchain4jdemo/controller/GitHookTest.java
+++ src/main/java/com/hailiang/langchain4jdemo/controller/GitHookTest.java
@@ -2,7 +2,6 @@ package com.hailiang.langchain4jdemo.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +9,6 @@ public class GitHookTest {
 
     @PostMapping("/test")
     public void Test(@RequestBody String test) {
-        System.out.println("git hook test:"+test);
+        System.out.println("git hook test-1:"+test);
     }
 }
