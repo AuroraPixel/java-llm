45642963166431b3fb9aa35135ad311768e8fb81
wanghanlin <wanghanlin@hailiang.com>
2024-06-18 18:07:43 +0800

    [init]:init git Pojo

--- src/main/java/com/hailiang/langchain4jdemo/pojo/gitlab/detail/DiffDetail.java
+++ src/main/java/com/hailiang/langchain4jdemo/pojo/gitlab/detail/DiffDetail.java
@@ -1,7 +1,12 @@
 package com.hailiang.langchain4jdemo.pojo.gitlab.detail;
 
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -55,4 +60,70 @@ public class DiffDetail {
      */
     @JsonProperty("deleted_file")
     private Boolean deletedFile;
+
+
+    /**
+     *  获取差异前的信息
+     * @return
+     */
+    public String getBeforeDiff() {
+        if (StrUtil.isEmpty(diff)) {
+            return "";
+        }
+        return getChangedBlock(diff, true);
+    }
+
+
+    /**
+     * 获取差异后的信息
+     * @return
+     */
+    public String getAfterDiff() {
+        if (StrUtil.isEmpty(diff)) {
+            return "";
+        }
+        return getChangedBlock(diff, false);
+    }
+
+    public static String getChangedBlock(String diff, boolean isOldVersion) {
+        StringBuilder result = new StringBuilder();
+        String[] diffLines = diff.split("\n");
+        int originalLineNumber = 0;
+        int newLineNumber = 0;
+        boolean inChangedBlock = false;
+
+        for (String line : diffLines) {
+            if (line.startsWith("@@")) {
+                // Parse the line numbers from the diff hunk header
+                String[] parts = line.split(" ");
+                String[] originalLineInfo = parts[1].split(",");
+                String[] newLineInfo = parts[2].split(",");
+
+                originalLineNumber = Integer.parseInt(originalLineInfo[0].substring(1));
+                newLineNumber = Integer.parseInt(newLineInfo[0].substring(1));
+
+                inChangedBlock = true;
+            } else if (inChangedBlock) {
+                if (line.startsWith(" ")) {
+                    result.append(isOldVersion ? originalLineNumber : newLineNumber).append("  ").append(line.substring(1)).append("\n");
+                    originalLineNumber++;
+                    newLineNumber++;
+                } else if (line.startsWith("-")) {
+                    if (isOldVersion) {
+                        result.append(originalLineNumber).append("- ").append(line.substring(1)).append("\n");
+                    }
+                    originalLineNumber++;
+                } else if (line.startsWith("+")) {
+                    if (!isOldVersion) {
+                        result.append(newLineNumber).append("+ ").append(line.substring(1)).append("\n");
+                    }
+                    newLineNumber++;
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+
 }
--- src/test/java/com/hailiang/langchain4jdemo/GitRemoteTests.java
+++ src/test/java/com/hailiang/langchain4jdemo/GitRemoteTests.java
@@ -2,6 +2,7 @@ package com.hailiang.langchain4jdemo;
 
 import com.hailiang.langchain4jdemo.pojo.gitlab.CommitInfo;
 import com.hailiang.langchain4jdemo.pojo.gitlab.MergeRequestInfo;
+import com.hailiang.langchain4jdemo.pojo.gitlab.detail.DiffDetail;
 import com.hailiang.langchain4jdemo.remote.GitLabRemote;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,13 @@ class GitRemoteTests {
     @Test
     void TestGitLabRemoteCompare(){
         CommitInfo compare = gitLabRemote.compare("915","5bf3349a67fc4743985be1feb2ccecf8ec5f792e", "1cc8ee2a9bd7147490cd5efcf62c4e4687ac7ea1");
-        System.out.println(compare);
+        System.out.println(compare.getDiffs().get(compare.getDiffs().size()-1).getDiff());
+        System.out.println("变更前");
+        System.out.println(compare.getDiffs().get(compare.getDiffs().size()-1).getBeforeDiff());
+        System.out.println("变更后");
+        System.out.println(compare.getDiffs().get(compare.getDiffs().size()-1).getAfterDiff());
+        System.out.println("在线网址");
+        System.out.println(compare.getWebUrl());
     }
 
     @Test
@@ -31,5 +38,23 @@ class GitRemoteTests {
     void TestGitLabRemoteSingleMR(){
         MergeRequestInfo compare = gitLabRemote.getSingleMR("915","4");
         System.out.println(compare);
+    }
+
+
+    @Test
+    void TestGetDiffCode(){
+        String diff = "@@ -22,7 +22,7 @@ public @interface ExternalApi {\\n\" +\n" +
+                "                      \"     String method() default \\\"GET\\\";\\n\" +\n" +
+                "                      \" \\n\" +\n" +
+                "                      \"     /**\\n\" +\n" +
+                "                      \"-     * 客户度名\\n\" +\n" +
+                "                      \"+     * 客户端名\\n\" +\n" +
+                "                      \"      * @return\\n\" +\n" +
+                "                      \"      */\\n\" +\n" +
+                "                      \"     String client();\\n";
+        DiffDetail diffDetail = new DiffDetail();
+        diffDetail.setDiff(diff);
+        System.out.println(diffDetail.getAfterDiff());
+        System.out.println(diffDetail.getBeforeDiff());
     }
 }
