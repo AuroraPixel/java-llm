e76b50c93d7e45e0a2f0da829040a01259938831
wanghanlin <wanghanlin@hailiang.com>
2024-06-19 17:47:07 +0800

    [init]:test request merge 9

--- src/main/java/com/hailiang/langchain4jdemo/service/impl/CodeReviewServiceImpl.java
+++ src/main/java/com/hailiang/langchain4jdemo/service/impl/CodeReviewServiceImpl.java
@@ -102,7 +102,7 @@ public class CodeReviewServiceImpl implements CodeReviewService {
         for (DiffDetail diffDetail : diffDetails) {
             codeList.add(diffDetail.getBeforeAndAfterDiff());
         }
-        log.debug("gpt开始对代码进行审查");
+        log.info("gpt开始对代码进行审查");
         String comment = codeReviewAgent.codeReview(codeList);
         log.debug("gpt对代码审查完成");
         CommentRequest commentRequest = new CommentRequest();
